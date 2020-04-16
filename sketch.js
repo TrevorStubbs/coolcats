b272fe1d392de77e5693a70fc04a60fddcdb1d9e
Trevor Stubbs <stubbste@gmail.com>
2020-04-16 12:22:17 -0700

    more updates

--- sketch.js
+++ sketch.js
@@ -3,14 +3,18 @@
 function setup() {
 
     
-    var cnv = createCanvas(100, 100);
-    var x = (windowWidth - width) / 2;
-    var y = (windowHeight - height) / 2;
-    cnv.position(x, y);
-    background(255, 0, 200);
+    // var cnv = createCanvas(100, 100);
+    // var x = (windowWidth - width) / 2;
+    // var y = (windowHeight - height) / 2;
+    // cnv.position(x, y);
+
+    // background(255, 0, 200);
     
-    createCanvas(100, 100);
+    var canvas = createCanvas(100, 100);
+    canvas.parent("sketchHolder");
     background(220);
+
+
   
     // ears
     triangle(58, 25, 72, 34, 80, 10);
