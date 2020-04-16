187695d86e36ce8277b37214282b28eaeb5a7eba
Trevor Stubbs <stubbste@gmail.com>
2020-04-15 18:06:05 -0700

    added js

---
+++ date.js
@@ -0,0 +1,6 @@
+var date = new Date();
+var today = date.getDate();
+var month = date.getMonth();
+var year = date.getYear();
+console.log()
+document.write("<h4>" + month + "/" + today + "/" + year + "</h4>");
