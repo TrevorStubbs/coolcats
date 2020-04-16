328b4d936a5616f0deced92bb62133afd266f865
Trevor Stubbs <stubbste@gmail.com>
2020-04-16 11:10:23 -0700

    made the buttons work

---
+++ button.js
@@ -0,0 +1,9 @@
+console.log("I am working?");
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
+
+function gotoPerson(){
+    location.href="https://amberfalbo.github.io/coolcats/";    
+}
