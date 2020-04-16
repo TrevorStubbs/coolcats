f8593be2da22748fba1905d5b813d8f26391c6c3
Trevor Stubbs <stubbste@gmail.com>
2020-04-16 12:10:52 -0700

    added funtions and image

--- agePrompt.js
+++ agePrompt.js
@@ -1,5 +1,20 @@
 // console.log("Am I connected?");
 
+// function ageQuestion(){
+//     var age = prompt("How old are you?");
+//     var catYears = age * 5;
+
+//     alert("If you were a cat you would be " + catYears + " old.");
+
+//     if (age > 60){
+//         document.write("<h5>" + "Cats are better than grandkids." + "</h5>");
+//     } else if (age > 30){
+//         document.write("<h5>" + "Cats are better than having kids." + "</h5>");
+//     }else{
+//         document.write("<h5>" + "Cats are better than parents." + "</h5>");
+//     }
+// }
+
 function ageQuestion(){
     var age = prompt("How old are you?");
     var catYears = age * 5;
@@ -7,11 +22,11 @@ function ageQuestion(){
     alert("If you were a cat you would be " + catYears + " old.");
 
     if (age > 60){
-        document.write("<h5>" + "Cats are better than grandkids." + "</h5>");
+        document.getElementById("catsAreBetter").innerHTML = ("Cats are better than grandkids.");
     } else if (age > 30){
-        document.write("<h5>" + "Cats are better than having kids." + "</h5>");
+        document.getElementById("catsAreBetter").innerHTML = ("Cats are better than having kids.");
     }else{
-        document.write("<h5>" + "Cats are better than parents." + "</h5>");
+        document.getElementById("catsAreBetter").innerHTML = ("Cats are better than parents.");
     }
 }
 
---
+++ sketch.js
@@ -0,0 +1,30 @@
+// draws a cat
+
+function setup() {
+
+    
+    var cnv = createCanvas(100, 100);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+    background(255, 0, 200);
+    
+    createCanvas(100, 100);
+    background(220);
+  
+    // ears
+    triangle(58, 25, 72, 34, 80, 10);
+    triangle(42, 25, 28, 34, 20, 10);
+  
+    // head
+    ellipse(50, 50, 60, 60);
+  
+    // right whiskers
+    line(70, 50, 90, 50);
+    line(70, 45, 90, 37);
+    line(70, 55, 90, 63);
+    // left whiskers
+    line(30, 50, 10, 50);
+    line(30, 45, 10, 37);
+    line(30, 55, 10, 63);
+  }
