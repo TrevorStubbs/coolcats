dc967dd8563c4236df64a6aa437040ff6ef25a9d
Trevor Stubbs <stubbste@gmail.com>
2020-04-16 16:54:43 -0700

    added more js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function displayDate(){
 
 //Age Question Function
 function ageQuestion(){
-    var age = prompt("How old are you?");
+    var age = prompt("How old are you? (please provide a number)");
     var catYears = age * 5;
 
     alert("If you were a cat you would be " + catYears + " old.");
@@ -39,5 +39,38 @@ function gotoPerson(){
     location.href="https://amberfalbo.github.io/coolcats/";    
 }
 
+//Cat Questions
+function catQuestion(){
+    var yesNo = 'nope';
+
+    while (( yesNo != 'YES') && (yesNo != "NO")){
+        yesNo = prompt("Do you like cats? (yes or no)").toUpperCase();
+        console.log(yesNo);
+    }
+
+    if(yesNo === 'NO'){
+        alert("Please be welcome!");
+    } else {
+        alert("YOU ARE ONE OF US!!!");
+    }
+}
+
+//Guessing game
+function guessGame(){
+    var number = Math.floor(Math.random()*10)+1;
+    var guess = 0;
+    
+    for(var i = 3; i >= 1; i--){
+        guess = Number(prompt('Cheshire cat "I will give you "' + i + '" tries to guess my number between 1 and 10."'))
+
+        if (guess === number){
+            alert("That is what I was thinking of.");
+            return;            
+        }
+    }
+    alert("Thank\'s for playing but you could not guess my number.")
+}
+
+catQuestion();
 displayDate();
 ageQuestion();
